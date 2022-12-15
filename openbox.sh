599c6527d04d401e95eb3742d7fea7de610bec46
Mobile-Linux-Official <108542674+Mobile-Linux-Official@users.noreply.github.com>
2022-12-16 01:36:39 +0700

    Create openbox.sh

---
+++ openbox.sh
@@ -0,0 +1,19 @@
+apt update -y
+apt install openbox feh dbus-x11 tigervnc-standalone-server -y
+
+mkdir -p ~/.vnc
+
+wget https://raw.githubusercontent.com/AndronixApp/AndronixOrigin/master/WM/wallpaper.jpg -O /usr/share/wallpaper.jpg
+echo "#!/bin/bash
+[ -r ~/.Xresources ] && xrdb ~/.Xresources
+export PULSE_SERVER=127.0.0.1
+export DISPLAY=:1
+XAUTHORITY=~/.Xauthority
+export XAUTHORITY                   
+openbox
+feh --bg-fill /usr/share/wallpaper.jpg" > ~/.vnc/xstartup                                           
+chmod +x ~/.vnc/xstartup
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-start -O /usr/local/bin/vncserver-start                       
+wget https://raw.githubusercontent.com/Techriz/AndronixOrigin/master/APT/LXDE/vncserver-stop -O /usr/local/bin/vncserver-stop
+chmod +x /usr/local/bin/vncserver-start
+chmod +x /usr/local/bin/vncserver-stop
